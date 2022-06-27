9912e01f4d291b642407853553f13a5a26e5d332
Nyi Nyi Myo Zin Htet <nyinyimyozin2@gmail.com>
2022-06-27 19:00:54 +0700

    Update Smart Contract and truffle test code

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -1,6 +1,85 @@
 pragma solidity ^0.5.0;
 
 contract Decentragram {
-  string public name = 'Decentragram';
-  
+    string public name = "Decentragram";
+
+    // Store Images
+    uint256 public imageCount = 0;
+    mapping(uint256 => Image) public images;
+
+    struct Image {
+        uint256 id;
+        string hash;
+        string description;
+        uint256 tipAmount;
+        address payable author;
+    }
+
+    event ImageCreated(
+        uint256 id,
+        string hash,
+        string description,
+        uint256 tipAmount,
+        address payable author
+    );
+
+    event ImageTipped(
+        uint256 id,
+        string hash,
+        string description,
+        uint256 tipAmount,
+        address payable author
+    );
+
+    // Create Images
+    function uploadImage(string memory _imgHash, string memory _description)
+        public
+    {
+        //Make sure the image hash exists
+        require(bytes(_imgHash).length > 0, "Put image");
+
+        // Make sure image description exists
+        require(bytes(_description).length > 0, "Put description");
+
+        //Make sure uploader address exists
+        require(msg.sender != address(0x0));
+
+        //Increment image id
+        imageCount++;
+
+        // Add Image to contract
+        images[imageCount] = Image(
+            imageCount,
+            _imgHash,
+            _description,
+            0,
+            msg.sender
+        );
+        // Trigger an event
+        emit ImageCreated(imageCount, _imgHash, _description, 0, msg.sender);
+    }
+
+    // Tip Images
+    function tipImageOwner(uint256 _id) public payable {
+        //Make sure the id is valid
+        require(_id > 0 && id_ <= imageCount, "image is invalid");
+        // Fetch the image
+        Image memory _image = image[_id];
+        // Fetch the author
+        address payable _author = _image.author;
+        // Pay the author by sending them Ether
+        address(_author).transfer(msg.value);
+        // Increment the top amount
+        _image.tipAmount = _image.tipAmount + msg.value;
+        // Update the image
+        images[_id] = _image;
+        // Trigger an event
+        emit ImageTipped(
+            _id,
+            _image.hash,
+            _image.description,
+            _image.tipAmount,
+            _author
+        );
+    }
 }
